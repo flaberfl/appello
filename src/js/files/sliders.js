3b367764be2059ae6e1a18381dc2333c3a979b9c
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2022-03-22 23:45:45 +0300

    kickstart end

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -63,8 +63,8 @@ function initSliders() {
 			observer: true,
 			observeParents: true,
 			slidesPerView: 5,
-			spaceBetween: 0,
-			autoHeight: true,
+			spaceBetween: 13,
+			// autoHeight: true,
 			speed: 800,
 			//touchRatio: 0,
 			//simulateTouch: false,
@@ -81,27 +81,26 @@ function initSliders() {
 				nextEl: '.kickstart__slide_next',
 				prevEl: '.kickstart__slide_prev',
 			},
-			/*
+
 			breakpoints: {
 				320: {
 					slidesPerView: 1,
 					spaceBetween: 0,
-					autoHeight: true,
+				},
+				480: {
+					slidesPerView: 2,
+					spaceBetween: 6,
 				},
 				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
+					slidesPerView: 4,
+					spaceBetween: 8,
 				},
 				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
+					slidesPerView: 5,
+					spaceBetween: 10,
 				},
 			},
-			*/
+
 			on: {
 
 			}
